f6021f144986933f13c7a0de431b6742f47781ac
Ali Raza <125049658+AliRaza-10@users.noreply.github.com>
2023-07-27 11:51:04 +0500

    Delete menu.py

--- menu.py
+++
@@ -1,8 +0,0 @@
-class menu:
-    def __init__(self,pizza_name,ingredients,discount):
-        self.pizza_name=pizza_name
-        self.ingredients=ingredients
-        self.discount = discount
-
-
-
